8040021796d0c0d94b07b1c0bef34e5a5e846904
Website  Opossum Optimizer by ICD <138514492+robinpaule@users.noreply.github.com>
2024-11-30 01:30:39 +0100

    Update index.js
    fix v2

--- js/index.js
+++ js/index.js
@@ -122,3 +122,146 @@ async function loadPage() {
         console.error("An error occurred during page load:", error);
     }
 }
+
+// Patterns for validation
+const namePattern = /^[a-zA-ZÀ-ÿ\s'-]+$/; // Allows letters (including accented), spaces, hyphens, apostrophes
+const institutionPattern = /^[a-zA-Z0-9À-ÿ\s'.,\-/&]+$/; // Allows letters, numbers, spaces, common punctuation
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+function validateInput(inputElement, pattern) {
+    const value = inputElement.value.trim();
+    if (value === '' || pattern.test(value)) {
+        inputElement.classList.remove('input-error');
+        return true;
+    } else {
+        inputElement.classList.add('input-error');
+        return false;
+    }
+}
+
+function checkButtonState() {
+    const firstNameInput = document.getElementById('firstName');
+    const lastNameInput = document.getElementById('lastName');
+    const institutionInput = document.getElementById('institution');
+    const userEmailInput = document.getElementById('userEmail');
+    const agreeCheckbox = document.getElementById('agreeTerms');
+    const generateButton = document.getElementById('generateButton');
+
+    // Validate inputs
+    const isFirstNameValid = validateInput(firstNameInput, namePattern);
+    const isLastNameValid = validateInput(lastNameInput, namePattern);
+    const isInstitutionValid = validateInput(institutionInput, institutionPattern);
+
+    const userEmail = userEmailInput.value.trim();
+    const isEmailValid = emailPattern.test(userEmail);
+
+    const isAgreed = agreeCheckbox.checked;
+
+    // Enable or disable the generate button
+    if (isFirstNameValid && isLastNameValid && isInstitutionValid && isEmailValid && isAgreed) {
+        generateButton.removeAttribute('disabled');
+    } else {
+        generateButton.setAttribute('disabled', true);
+    }
+}
+
+// Event listeners for input fields
+document.getElementById('firstName').addEventListener('input', checkButtonState);
+document.getElementById('lastName').addEventListener('input', checkButtonState);
+document.getElementById('institution').addEventListener('input', checkButtonState);
+document.getElementById('userEmail').addEventListener('input', checkButtonState);
+document.getElementById('agreeTerms').addEventListener('change', checkButtonState);
+
+// Consent privacy button
+const consentPrivacyButton = document.getElementById('consentPrivacyButton');
+consentPrivacyButton.addEventListener('click', function() {
+    document.cookie = "cookieConsent=true; path=/; max-age=" + (60 * 60 * 24 * 365); // Set cookie for 1 year
+    loadPage();
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!checkCookieConsent()) {
+        document.getElementById('popupPrivacyConsent').style.display = 'flex';
+        document.body.classList.add('body-no-scroll');
+        checkButtonState();
+    } else {
+        checkButtonState();
+        handleMenuNavigation();
+        loadPage();
+    }
+});
+
+// Copy license key after successful signup
+document.getElementById('copyButton').addEventListener('click', function() {
+    const licenseKey = document.getElementById('generatedLicenseKey').textContent;
+    copyToClipboard(licenseKey);
+    document.getElementById("clipboardC").style.display = "none";
+    document.getElementById("checkmarkC").style.display = "block";
+});
+
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(function() {
+        document.getElementById('copyButtonText').textContent = 'Copied';
+    }).catch(function(err) {
+        console.error('Failed to copy text: ', err);
+    });
+}
+
+// FAQ Events
+document.addEventListener('DOMContentLoaded', function() {
+    var faqQuestions = document.querySelectorAll('.faq-question');
+
+    faqQuestions.forEach(function(question) {
+        question.addEventListener('click', function() {
+            var answer = this.nextElementSibling;
+            var svgArrow = this.querySelector('.svg-icon');
+
+            answer.classList.toggle('active');
+            var isOpen = answer.classList.contains('active');
+
+            answer.style.maxHeight = isOpen ? answer.scrollHeight + 20 + "px" : null;
+
+            this.setAttribute('aria-expanded', isOpen ? 'true' : 'false');
+
+            svgArrow.style.transform = isOpen ? 'rotate(180deg)' : 'rotate(0deg)';
+            svgArrow.style.transition = 'transform 0.5s ease';
+        });
+    });
+});
+
+// Handle menu navigation
+function handleMenuNavigation() {
+    const hash = window.location.hash;
+    if (hash) {
+        const targetSection = document.querySelector(hash);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', handleMenuNavigation);
+window.addEventListener('hashchange', handleMenuNavigation);
+
+// Fetch Opossum Download Count
+function updateDownloadCount() {
+    return new Promise((resolve, reject) => {
+        const downloadCountRef = ref(db, 'downloadCount');
+        get(downloadCountRef).then((snapshot) => {
+            if (snapshot.exists()) {
+                const data = snapshot.val();
+                document.getElementById('download-count').textContent = data.count;
+                resolve();
+            } else {
+                console.log('No download count data available.');
+                reject('No data available');
+            }
+        }).catch((error) => {
+            console.error('Error fetching download count:', error);
+            reject(error);
+        });
+    });
+}
